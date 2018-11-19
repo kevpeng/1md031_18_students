50f8b7acb82c2b2123a888e63f2370b64afe0951
Kevin Peng <kevin.peng@richmond.edu>
2018-11-19 16:47:45 +0100

    got the dots!
    functionality added

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,11 +1,14 @@
+'use strict';
+var socket = io();
+
 var vm = new Vue({
     el: '#myID',
     data: {
         burgers: food,
         customer_details: [],
-        order: []
-
-
+        orders: [],
+            x: 0,
+            y: 0
     },
     methods: {
         submit: function(id) {
@@ -15,6 +18,7 @@ var vm = new Vue({
             // var street_name = document.getElementById("Street_name").value;
             var payment_mthd = document.getElementById("Payment").value;
             var gender = document.querySelector('input[name="gender"]:checked').value;
+            var deliver_to = 0;
 
             var burgers_selected = [];
             for(var burger in this.burgers) {
@@ -27,17 +31,42 @@ var vm = new Vue({
             var array = [
                 name,
                 email,
+                deliver_to,
                 // street_num + ' ' +
                 // street_name,
                 payment_mthd,
                 gender
             ]
             this.customer_details = array;
-            this.order = burgers_selected;
+            this.orders = burgers_selected;
             var e = document.getElementById(id);
             e.style.display='block';
 
+        },
+        displayOrder: function(event) {
+            var offset = {x: event.currentTarget.getBoundingClientRect().left,
+                y: event.currentTarget.getBoundingClientRect().top};
+            this.x = event.clientX - 10 - offset.x;
+            this.y = event.clientY - 10 - offset.y;
+            console.log(offset);
+            console.log(this.x + ' ' + this.y);
+        },
+        getNext: function () {
+            var lastOrder = Object.keys(this.orders).reduce(function (last, next) {
+                return Math.max(last, next);
+            }, 0);
+            return lastOrder + 1;
+        },
+        addOrder: function (event) {
+            socket.emit("addOrder", { orderId: this.getNext(),
+                details: { x: event.clientX - 10 - this.x,
+                    y: event.clientY - 10 - this.y },
+                orderItems: this.orders
+
+            });
         }
+
+
 
     }
 
